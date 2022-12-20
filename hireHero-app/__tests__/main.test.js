fb290c992b5a8f2d1f456fa3f3b17c62897c5825
Elise <elisemcconnell11@gmail.com>
2022-12-20 15:21:38 -0600

    initialized test suite Co-authored-by: Joseph Hynan 111455487+JoeH1020@users.noreply.github.com Co-authored-by: Elise McConnell 112969060+enmcco@users.noreply.github.com Co-authored-by: Ruqayaah Sabitu 49168086+ruqayaahh@users.noreply.github.com Co-authored-by: Tiffany Chau 102318022+tiffanynchau@users.noreply.github.com Co-authored-by: Ed Cho 106274379+edcho720@users.noreply.github.com

---
+++ hireHero-app/__tests__/main.test.js
@@ -0,0 +1,24 @@
+/* JEST TESTS
+login page:
+-renders username entry field
+-renders password entry field
+-displays error text if user submits with an empty username or password field
+-displays error text if username and password combination is incorrect
+-redirects to dashboard if login is successful
+
+signup page:
+-renders username entry field
+-renders password entry field
+-displays error text if user submits with an empty username or password field
+-displays error text if username and password combination is incorrect
+-redirects to dashboard if signup is successful
+
+dashboard page:
+-renders button to log a job application
+-renders a table with job application records
+-updates the table when a job application is logged
+
+SUPERTESTS
+
+
+*/
---
+++ hireHero-app/src/sum.ts
@@ -0,0 +1,8 @@
+function sum(a: number, b: number) {
+  return a + b;
+}
+
+module.exports = sum;
+
+// VSCode told me this was needed...not sure why
+export {};
